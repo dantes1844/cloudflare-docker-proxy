8e29245690c1318128e536591fe8b932dab6c40f
dantes1844 <dantes1844@hotmail.com>
2024-12-18 08:57:23 +0800

    增加说明页

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+import DOCS from './index.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -39,8 +42,22 @@ async function handleRequest(request) {
       }
     );
   }
+ 
+
+
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
+
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
+  }
+
   if (url.pathname == "/v2/") {
     const newUrl = new URL(upstream + "/v2/");
     const headers = new Headers();
